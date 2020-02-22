21b6170e7659b9549cd8cfbedb9e5d267fa7dd04
Brad Taylor <brad.taylor@gmail.com>
2020-02-22 12:28:14 -0800

    Rewrite plugin to use Platform instead of accessory

--- index.js
+++ index.js
@@ -1,57 +1,94 @@
 "use strict";
 
+const PLUGIN_NAME = "homebridge-elgato-key-light";
+const PLATFORM_NAME = "ElgatoKeyLight";
+
 const elgato = require("elgato-light-api");
 
-var Service, Characteristic, HomebridgeAPI;
+var HAP, Service, Accessory;
 
 module.exports = function(homebridge) {
+  HAP = homebridge.hap;
   Service = homebridge.hap.Service;
-  Characteristic = homebridge.hap.Characteristic;
-  HomebridgeAPI = homebridge;
-  homebridge.registerAccessory("homebridge-elgato-key-light", "ElgatoKeyLight", ElgatoKeyLight);
+  Accessory = homebridge.platformAccessory;
+
+  homebridge.registerPlatform(PLUGIN_NAME, PLATFORM_NAME, ElgatoKeyLight, true);
 }
 
-function ElgatoKeyLight(log, config) {
+function ElgatoKeyLight(log, config, platform) {
   this.log = log;
-  this.disabled = config.disabled || false;
-
-  if (this.disabled) {
+  this.config = config;
+  this.platform = platform;
+  this.accessories = {};
+        
+  if (!this.platform) {
     return;
   }
 
   this.lightAPI = new elgato.ElgatoLightAPI();
-  this.lightAPI.on('newLight', function (newLight) {
-    log(newLight.name + " detected");
+  this.platform.on('didFinishLaunching', () => {
+    this.lightAPI.on('newLight', (light) => this.registerAccessory(light, config));
   });
 }
 
-ElgatoKeyLight.prototype.getServices = function() {
-  if (this.disabled) {
-    return [];
-  }
-
-  var services = [];
-  for (var i in this.lightAPI.keyLights) {
-    var light = this.lightAPI.keyLights[i];
-
-    // NOTE: I've never seen more than 1 sub-light in the wild
-    var status = light.options.lights[0];
-
-    var service = new Service.Lightbulb(name);
-    service.getCharacteristic(Characteristic.On)
-           .on('set', function (on, callback) {
-      this.lightAPI.updateLightOptions(light, {
-        numberOfLights: 1, lights: [ { on: on } ]
-      });
-
-      callback();
-    });
-
-    service.setCharacteristic(Characteristic.On, status.on);
-    service.setCharacteristic(Characteristic.Brightness, status.brightness);
-    service.setCharacteristic(Characteristic.ColorTemperature, status.temperature);
-    services.push(service);
-  }
-
-  return services;
+ElgatoKeyLight.prototype.configureAccessory = function(accessory) {
+  this.accessories[accessory.UUID] = accessory;
 }
+
+ElgatoKeyLight.prototype.registerAccessory = function(light, config) {
+  let uuid = HAP.uuid.generate(light.info.serialNumber);
+  let accessory = this.accessories[uuid];
+  if (!accessory) {
+    accessory = new Accessory(light.name, uuid);
+    this.accessories[uuid] = accessory;
+
+    this.platform.registerPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [accessory]);
+  }
+
+  this.registerServices(light, accessory, config);
+  this.log(`Found ${light.name}`);
+}
+
+ElgatoKeyLight.prototype.registerServices = function(light, accessory, config) {
+  accessory.getService(Service.AccessoryInformation)
+    .setCharacteristic(HAP.Characteristic.Manufacturer, "Elgato")
+    .setCharacteristic(HAP.Characteristic.Model, light.info.productName)
+    .setCharacteristic(HAP.Characteristic.SerialNumber, light.info.serialNumber);
+
+  let service = accessory.getService(Service.Lightbulb);
+  if (!service) {
+    accessory.addService(Service.LightBulb, light.name);
+  }
+
+  var status = light.options.lights[0];
+  service.setCharacteristic(HAP.Characteristic.On, status.on);
+  service.getCharacteristic(HAP.Characteristic.On)
+         .on('set', (on, callback) => {
+    this.log("Setting Characteristic.On to " + on);
+    this.lightAPI.updateLightOptions(light, {
+      numberOfLights: 1, lights: [ { on: on } ]
+    });
+    callback(null, on);
+  });
+
+  service.setCharacteristic(HAP.Characteristic.Brightness, status.brightness);
+  service.getCharacteristic(HAP.Characteristic.Brightness)
+         .on('set', (brightness, callback) => {
+    this.log("Setting Characteristic.Brightness to " + brightness);
+    this.lightAPI.updateLightOptions(light, {
+      numberOfLights: 1, lights: [ { brightness: brightness } ]
+    });
+    callback(null, brightness);
+  });
+
+  service.setCharacteristic(HAP.Characteristic.ColorTemperature, status.temperature);
+  service.getCharacteristic(HAP.Characteristic.ColorTemperature)
+         .on('set', (temperature, callback) => {
+    this.log("Setting Characteristic.ColorTemperature to " + temperature);
+    this.lightAPI.updateLightOptions(light, {
+      numberOfLights: 1, lights: [ { temperature: temperature } ]
+    });
+    callback(null, temperature);
+  });
+}
+
